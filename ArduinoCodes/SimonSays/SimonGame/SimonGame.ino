a8c6539ef81bf9cff46427d62855d8254dc85cdf
SirArtemios <mr.sirartemios@gmail.com>
2022-12-24 14:49:38 +0100

    SimonGame Finished !
    Need to add 433MHz part and wifi transmission

--- ArduinoCodes/SimonSays/SimonGame/SimonGame.ino
+++ ArduinoCodes/SimonSays/SimonGame/SimonGame.ino
@@ -13,7 +13,18 @@ const int BUZZER = 2;
 const int RESET = 13;
 
 const char* codes[] = {"4,5,3", "4,5,3,6,5,4,5", "4,5,3,6,5,4,5,6,3,4,6", "4,5,3,6,5,4,5,6,3,4,6,4,6,3,6,4", "4,5,3,6,5,4,5,6,3,4,6,4,6,3,6,4,5,3,6,5"};
-int buzz[] = {300, 350, 400, 450};
+struct Note {
+  int frequency;
+  char code;
+};
+Note notes[] = {
+  {300, '3'},
+  {350, '4'},
+  {400, '5'},
+  {450, '6'}
+};
+
+unsigned long startTime;
 
 void setup() {
   Serial.begin(9600);
@@ -30,84 +41,87 @@ void setup() {
 }
 
 
-void play_note(int note, int notespeed) {
-  analogWrite(BUZZER, 255);
-  tone(BUZZER, note, notespeed);
-  delay(notespeed * 2);
-  noTone(BUZZER);
-  analogWrite(BUZZER, 0);
+void play_note(char code) {
+  for (int i = 0; i<4; i++) {
+    if (notes[i].code == code) {
+      tone(BUZZER, notes[i].frequency, 50);
+      delay(100);
+      noTone(BUZZER);
+      break;
+    }
+  }
 }
 
 
 void boutons() {
   if (digitalRead(GREEN_BTN) == LOW) {
     reponse += 3;
-    play_note(300, 50);
+    play_note('3');
     digitalWrite(GREEN, HIGH);
     delay(100);
     digitalWrite(GREEN, LOW);
     Serial.println(reponse);
-    delay(1000);
+    startTime = millis();
+    delay(500);
   }
 
   else if (digitalRead(BLUE_BTN) == LOW) {
     reponse += 4;
-    play_note(350, 50);
+    play_note('4');
     digitalWrite(BLUE, HIGH);
     delay(100);
     digitalWrite(BLUE, LOW);
     Serial.println(reponse);
-    delay(1000);
+    startTime = millis();
+    delay(500);
   }
     
   else if (digitalRead(YELLOW_BTN) == LOW) {
     reponse += 5;
-    play_note(400, 50);
+    play_note('5');
     digitalWrite(YELLOW, HIGH);
     delay(100);
     digitalWrite(YELLOW, LOW);
     Serial.println(reponse);
-    delay(1000);
+    startTime = millis();
+    delay(500);
   }
     
   else if (digitalRead(RED_BTN) == LOW) {
     reponse += 6;
-    play_note(450, 50);
+    play_note('6');
     digitalWrite(RED, HIGH);
     delay(100);
     digitalWrite(RED, LOW);
     Serial.println(reponse);
-    delay(1000);
+    startTime = millis();
+    delay(500);
   }
 }
 
 
 void animationSuccess() {
   digitalWrite(GREEN, HIGH); 
-  delay(50);
+  play_note('3'); 
   digitalWrite(GREEN, LOW);
-  play_note(300, 50); 
   digitalWrite(BLUE, HIGH); 
-  delay(50);
+  play_note('4');
   digitalWrite(BLUE, LOW);
-  play_note(350, 50);
   digitalWrite(YELLOW, HIGH);
-  delay(50);
+  play_note('5');
   digitalWrite(YELLOW, LOW);
-  play_note(400, 50);
   digitalWrite(RED, HIGH); 
-  delay(50);
+  play_note('6');
   digitalWrite(RED, LOW);
-  play_note(450, 50);
   delay(100);
 
   digitalWrite(GREEN, HIGH);
   digitalWrite(BLUE, HIGH);
   digitalWrite(YELLOW, HIGH);
   digitalWrite(RED, HIGH);
-  play_note(500, 50);
-  delay(50);
-
+  tone(BUZZER, 450, 100);
+  delay(200);
+  noTone(BUZZER);
   digitalWrite(GREEN, LOW);
   digitalWrite(BLUE, LOW);
   digitalWrite(YELLOW, LOW);
@@ -117,60 +131,70 @@ void animationSuccess() {
 
 void doCode(char* code_source) {
 
-  char code[sizeof(code_source)];
-
-  strcpy(code, code_source);
+  char* code = strdup(code_source);
 
   String codeTest(code);
   codeTest.replace(",","");
 
   char* codeArr = strtok(code, ",");
 
+  startTime = millis();
+
   while (codeArr != NULL) {
-    digitalWrite(atoi(codeArr), HIGH);
-    play_note(buzz[atoi(codeArr)-3], 50);
-    delay(500);
-    digitalWrite(atoi(codeArr), LOW);
+    for (int i = 0; i<4; i++) {
+      if (notes[i].code == *codeArr) {
+        tone(BUZZER, notes[i].frequency, 50);
+        digitalWrite(atoi(codeArr), HIGH);
+        delay(500);
+        digitalWrite(atoi(codeArr), LOW);
+        break;
+      }
+    }
     codeArr = strtok(NULL, ",");
   }
 
-  while(reponse.length() != codeTest.length()) {
+  free(code);
+  reponse = "";
+
+  while (reponse.length() != codeTest.length()) {
     boutons();
+    if(millis() - startTime >= 10000) {
+      break;
+    }
   }
 
-  if(reponse == codeTest) {
+  if (reponse == codeTest) {
     animationSuccess();
   }
 
-  else if(reponse != codeTest){
+  else if (reponse != codeTest){
     digitalWrite(RED, HIGH);
-    play_note(200, 500);
+    tone(BUZZER, 200, 250);
+    delay(500);
+    noTone(BUZZER);
     digitalWrite(RED, LOW);
+    reponse = "";
     y = 0;
   }
 }
 
 
 void win() {
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(BLUE, HIGH);
-  digitalWrite(YELLOW, HIGH);
-  digitalWrite(RED, HIGH);
-  play_note(500, 50);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, LOW);
-  digitalWrite(YELLOW, LOW);
-  digitalWrite(RED, LOW);
-  delay(500);
-  digitalWrite(GREEN, HIGH);
-  digitalWrite(BLUE, HIGH);
-  digitalWrite(YELLOW, HIGH);
-  digitalWrite(RED, HIGH);
-  play_note(500, 50);
-  digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, LOW);
-  digitalWrite(YELLOW, LOW);
-  digitalWrite(RED, LOW);
+  delay(1000);
+  for (int i = 0; i<4; i++) {
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(BLUE, HIGH);
+    digitalWrite(YELLOW, HIGH);
+    digitalWrite(RED, HIGH);
+    tone(BUZZER, 500, 250);
+    delay(500);
+    noTone(BUZZER);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+    delay(500);
+  }
 }
 
 
@@ -190,32 +214,31 @@ void jeu() {
       digitalWrite(GREEN, HIGH); 
       delay(75);
       digitalWrite(GREEN, LOW);
-      play_note(300, 50); 
+      play_note('3'); 
       digitalWrite(BLUE, HIGH); 
       delay(75);
       digitalWrite(BLUE, LOW);
-      play_note(350, 50);
+      play_note('4');
       digitalWrite(YELLOW, HIGH);
       delay(75);
       digitalWrite(YELLOW, LOW);
-      play_note(400, 50);
+      play_note('5');
       digitalWrite(RED, HIGH); 
       delay(75);
       digitalWrite(RED, LOW);
-      play_note(450, 50);
+      play_note('6');
     }
     y = 1;
 
-    for(int cds = 0; cds < 5; cds++) {
+    for (int cds = 0; cds < 5; cds++) {
       delay(1000);
       Serial.println(codes[cds]);
       doCode(codes[cds]);
-      reponse = "";
-      if(y == 0) {
+      if (y == 0) {
         break;
       }
     }
-    if(y != 0) {
+    if (y != 0) {
       y = 0;
       win();
     }
